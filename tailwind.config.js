952accdeeb1e0242a700891bc7f44df5e230fba9
santoshdeo <semtitans.santosh@gmail.com>
2025-01-24 22:38:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/*.*",
+    "./sections/*.*",
+    "./snippets/*.*",
+    "./templates/*.*",
+    "./templates/**/*.*"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
